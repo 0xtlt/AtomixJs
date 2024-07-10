c8d95b8f1500e28003824b2267aa31559ef61ca2
Thomas <31560900+0xtlt@users.noreply.github.com>
2024-07-10 17:00:49 +0200

    Update atomixComponent.ts

--- src/atomixComponent.ts
+++ src/atomixComponent.ts
@@ -1,176 +1,184 @@
-import { type AddCycleFn } from "./cycle";
+import type { AddCycleFn } from "./cycle";
 
 export const AHTMLElement = makeAtomixComponent(HTMLElement);
 export type ComponentType = InstanceType<typeof AHTMLElement>;
 
 export function makeAtomixComponent<
-  Element extends {
-    new (...args: any[]): HTMLElement;
-  }
+	Element extends {
+		new (...args: unknown[]): HTMLElement;
+	},
 >(element: Element) {
-  return class extends element {
-    // Cycles system
-    #_cycles: Array<() => any> = [];
-    #_namedCycles: Record<string, () => any> = {};
-    lazyload: boolean = false;
-    loaded: boolean = false;
+	return class extends element {
+		// Cycles system
+		#_cycles: Array<() => unknown> = [];
+		#_namedCycles: Record<string, () => unknown> = {};
+		lazyload = false;
+		loaded = false;
 
-    constructor(...args: any[]) {
-      super(...args);
-    }
+		connectedCallback() {
+			if (!this.dataset["client:load"]) {
+				const observer = new IntersectionObserver(
+					(entries) => {
+						for (const entry of entries) {
+							if (entry.isIntersecting) {
+								observer.unobserve(this);
+								this.initComponent();
+							}
+						}
+					},
+					{ threshold: 0.1 },
+				);
 
-    connectedCallback() {
-      if (this.lazyload) {
-        const observer = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                observer.unobserve(this);
-                this.initComponent();
-              }
-            });
-          },
-          { threshold: 0.1 }
-        );
+				observer.observe(this);
 
-        observer.observe(this);
+				return;
+			}
 
-        return;
-      }
+			setTimeout(() => this.initComponent(), 1);
+		}
 
-      setTimeout(() => this.initComponent(), 1);
-    }
+		disconnectedCallback() {
+			if (this.loaded) {
+				this.onDestroy?.();
+				this.clearCycles?.();
+				this.loaded = false;
+			}
+		}
 
-    disconnectedCallback() {
-      if (this.loaded) {
-        this.onDestroy?.();
-        this.clearCycles?.();
-        this.loaded = false;
-      }
-    }
+		initComponent() {
+			if (this.loaded) return;
 
-    initComponent() {
-      if(this.loaded) return;
+			this.loaded = true;
+			this.onStart?.();
+		}
 
-      this.loaded = true;
-      this.onStart?.();
-    }
+		stateSubscribe<Type>(
+			stateSubscribeCallback: (
+				cb: (value: Type, oldValue: Type) => void | Promise<void>,
+			) => () => void,
+			callback: (value: Type, oldValue: Type) => void | Promise<void>,
+		) {
+			this.cycle(() => stateSubscribeCallback(callback));
+		}
 
-    stateSubscribe<Type>(
-      stateSubscribeCallback: (
-        cb: (value: Type, oldValue: Type) => void | Promise<void>
-      ) => () => void,
-      callback: (value: Type, oldValue: Type) => void | Promise<void>
-    ) {
-      this.cycle(() => stateSubscribeCallback(callback));
-    }
+		// Cycles
+		cycle(cb: AddCycleFn) {
+			const fn = cb();
 
-    // Cycles
-    cycle(cb: AddCycleFn) {
-      let fn = cb();
+			if (fn instanceof Promise) {
+				fn.then((fn) => this.#_cycles.push(fn));
+			} else {
+				this.#_cycles.push(fn);
+			}
+		}
 
-      if (fn instanceof Promise) {
-        fn.then((fn) => this.#_cycles.push(fn));
-      } else {
-        this.#_cycles.push(fn);
-      }
-    }
+		clearCycles() {
+			this.#_cycles.map((cb) => {
+				const res = cb();
+				if (res instanceof Promise) {
+					res.then(() => null);
+				}
+			});
 
-    clearCycles() {
-      this.#_cycles.map((cb) => {
-        const res = cb();
-        if (res instanceof Promise) {
-          res.then(() => null);
-        }
-      });
+			const keys = Object.keys(this.#_namedCycles);
+			// unmount named cycles
+			for (const key of keys) {
+				const res = this.#_namedCycles[key]();
+				if (res instanceof Promise) {
+					res.then(() => null);
+				}
+			}
 
-      const keys = Object.keys(this.#_namedCycles);
-      // unmount named cycles
-      keys.forEach((key) => {
-        const res = this.#_namedCycles[key]();
-        if (res instanceof Promise) {
-          res.then(() => null);
-        }
-      });
+			this.#_cycles = [];
+			this.#_namedCycles = {};
+		}
 
-      this.#_cycles = [];
-      this.#_namedCycles = {};
-    }
+		// Named cycles
+		namedCycle(name: string, cb: AddCycleFn) {
+			// unmout previous cycle
+			if (this.#_namedCycles[name]) {
+				const res = this.#_namedCycles[name]();
+				if (res instanceof Promise) {
+					res.then(() => null);
+				}
+			}
+			const fn = cb();
 
-    // Named cycles
-    namedCycle(name: string, cb: AddCycleFn) {
-      // unmout previous cycle
-      if (this.#_namedCycles[name]) {
-        const res = this.#_namedCycles[name]();
-        if (res instanceof Promise) {
-          res.then(() => null);
-        }
-      }
-      let fn = cb();
+			if (fn instanceof Promise) {
+				fn.then((fn) => (this.#_namedCycles[name] = fn));
+			} else {
+				this.#_namedCycles[name] = fn;
+			}
+		}
 
-      if (fn instanceof Promise) {
-        fn.then((fn) => (this.#_namedCycles[name] = fn));
-      } else {
-        this.#_namedCycles[name] = fn;
-      }
-    }
+		// Auto cycled events
+		on<K extends keyof HTMLElementEventMap>(
+			type: K,
+			listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => unknown,
+			options?: boolean | AddEventListenerOptions,
+		): void;
 
-    // Auto cycled events
-    on<K extends keyof HTMLElementEventMap>(
-      type: K,
-      listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
-      options?: boolean | AddEventListenerOptions
-    ): void;
-    
-    on(
-      type: string,
-      listener: (this: HTMLElement, ev: CustomEvent) => any,
-      options?: boolean | AddEventListenerOptions
-    ): void;
-    
-    on<K extends keyof HTMLElementEventMap | string>(
-      type: K,
-      listener: (this: HTMLElement, ev: K extends keyof HTMLElementEventMap ? HTMLElementEventMap[K] : CustomEvent) => any,
-      options?: boolean | AddEventListenerOptions
-    ) {
-      this.cycle(() => {
-        this.addEventListener(type, listener as EventListener, options);
-        return () => this.removeEventListener(type, listener as EventListener, options);
-      });
-    }
-    
-    onElement<K extends keyof HTMLElementEventMap>(
-      element: HTMLElement | Document | Window,
-      type: K,
-      listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
-      options?: boolean | AddEventListenerOptions
-    ): void;
-    
-    onElement(
-      element: HTMLElement | Document | Window,
-      type: string,
-      listener: (this: HTMLElement, ev: CustomEvent) => any,
-      options?: boolean | AddEventListenerOptions
-    ): void;
-    
-    onElement<K extends keyof HTMLElementEventMap | string>(
-      element: HTMLElement | Document | Window,
-      type: K,
-      listener: (this: HTMLElement, ev: K extends keyof HTMLElementEventMap ? HTMLElementEventMap[K] : CustomEvent) => any,
-      options?: boolean | AddEventListenerOptions
-    ) {
-      this.cycle(() => {
-        element.addEventListener(type, listener as EventListener, options);
-        return () => element.removeEventListener(type, listener as EventListener, options);
-      });
-    }
+		on(
+			type: string,
+			listener: (this: HTMLElement, ev: CustomEvent) => unknown,
+			options?: boolean | AddEventListenerOptions,
+		): void;
 
-    // Lifecycle
-    onStart?(): void;
-    onDestroy?(): void;
+		on<K extends keyof HTMLElementEventMap | string>(
+			type: K,
+			listener: (
+				this: HTMLElement,
+				ev: K extends keyof HTMLElementEventMap
+					? HTMLElementEventMap[K]
+					: CustomEvent,
+			) => unknown,
+			options?: boolean | AddEventListenerOptions,
+		) {
+			this.cycle(() => {
+				this.addEventListener(type, listener as EventListener, options);
+				return () =>
+					this.removeEventListener(type, listener as EventListener, options);
+			});
+		}
 
-    logger(...args: any[]) {
-      console.info(`[${this.tagName}]`, ...args);
-    }
-  };
+		onElement<K extends keyof HTMLElementEventMap>(
+			element: HTMLElement | Document | Window,
+			type: K,
+			listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => unknown,
+			options?: boolean | AddEventListenerOptions,
+		): void;
+
+		onElement(
+			element: HTMLElement | Document | Window,
+			type: string,
+			listener: (this: HTMLElement, ev: CustomEvent) => unknown,
+			options?: boolean | AddEventListenerOptions,
+		): void;
+
+		onElement<K extends keyof HTMLElementEventMap | string>(
+			element: HTMLElement | Document | Window,
+			type: K,
+			listener: (
+				this: HTMLElement,
+				ev: K extends keyof HTMLElementEventMap
+					? HTMLElementEventMap[K]
+					: CustomEvent,
+			) => unknown,
+			options?: boolean | AddEventListenerOptions,
+		) {
+			this.cycle(() => {
+				element.addEventListener(type, listener as EventListener, options);
+				return () =>
+					element.removeEventListener(type, listener as EventListener, options);
+			});
+		}
+
+		// Lifecycle
+		onStart?(): void;
+		onDestroy?(): void;
+
+		logger(...args: unknown[]) {
+			console.info(`[${this.tagName}]`, ...args);
+		}
+	};
 }
